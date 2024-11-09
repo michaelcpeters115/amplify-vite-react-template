accfd11a6d62be270a01c45d8026ae933c226fe4
michaelcpeters115 <123867234+michaelcpeters115@users.noreply.github.com>
2024-11-09 09:35:55 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,17 @@
+// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfLjakU2aiJxF7z7NQyoK60augTtgTbZk",
-  authDomain: "AIzaSyAfLjakU2aiJxF7z7NQyoK60augTtgTbZk",
-  projectId: "AIzaSyAfLjakU2aiJxF7z7NQyoK60augTtgTbZk",
-  storageBucket: "limitx-debe2.firebasestorage.app",
-  messagingSenderId: "809209726619",
+  aapiKey: "AIzaSyAfLjakU2aiJxF7z7NQyoK60augTtgTbZk", 
+  authDomain: "AIzaSyAfLjakU2aiJxF7z7NQyoK60augTtgTbZk", 
+  projectId: "AIzaSyAfLjakU2aiJxF7z7NQyoK60augTtgTbZk", 
+  storageBucket: "limitx-debe2.firebasestorage.app", 
+  messagingSenderId: "809209726619", 
   appId: "1:809209726619:web:d2ea146e7e3ff6f56ca83e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
