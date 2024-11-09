cc1f872c4bcb0e570623fb2013868098bfdfbc03
michaelcpeters115 <123867234+michaelcpeters115@users.noreply.github.com>
2024-11-09 09:01:23 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-Amplify.configure(outputs);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
